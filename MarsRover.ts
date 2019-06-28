3db8663ebfc02b14f97f940fe18e556af97a8475
neiltownsley <n31LT0wn5l3y>
2019-06-28 10:19:02 +0100

    update parameter order for MarsRover

--- MarsRover.ts
+++ MarsRover.ts
@@ -5,23 +5,23 @@ import {Plateau} from "./Plateau";
 import {StringCommandParser} from "./StringCommandParser";
 
 export class MarsRover {
-  private currentCoordinates: Coordinates;
-  private currentDirection: Direction;
-  private plateau: Plateau;
+    private currentCoordinates: Coordinates;
+    private currentDirection: Direction;
+    private plateau: Plateau;
 
-  constructor(currentCoordinates: Coordinates, currentDirection: Direction, plateau: Plateau) {
-    this.currentCoordinates = currentCoordinates;
-    this.currentDirection = currentDirection;
-    this.plateau = plateau;
-  }
+    constructor(plateau: Plateau, currentCoordinates: Coordinates, currentDirection: Direction) {
+        this.plateau = plateau;
+        this.currentCoordinates = currentCoordinates;
+        this.currentDirection = currentDirection;
+    }
 
-  public run = (commandString: string): ICommand[] => {
-    const roverCommands = new StringCommandParser(commandString).toCommands();
+    public run = (commandString: string): ICommand[] => {
+        const roverCommands = new StringCommandParser(commandString).getCommands();
 
-    roverCommands.forEach(
-      (command: ICommand) => {
-        command.execute(this);
-      });
-    return roverCommands;
-  }
+        roverCommands.forEach(
+            (command: ICommand) => {
+                command.execute(this);
+            });
+        return roverCommands;
+    }
 }
